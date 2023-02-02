fbd9da169ee4c0936fbcccec9c4ce805206ecd8c
John Ruble <john.ruble@atomicobject.com>
2023-02-02 13:56:24 -0500

    repro.tsx

---
+++ src/pages/repro.tsx
@@ -0,0 +1,13 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { InferGetServerSidePropsType } from "next";
+
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async ({ query, req, res }) => {
+    if ((query.foo = "bar")) {
+      return { notFound: true };
+    }
+    return { props: { hello: "world" } };
+  },
+});
+
+export type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
